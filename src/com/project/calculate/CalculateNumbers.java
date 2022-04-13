681a429e95857c151a566d57710be50915a683bb
AshankMishra <ashankkumarmishra@gmail.com>
2022-04-13 09:59:22 +0530

    Project Setup

---
+++ src/com/project/calculate/CalculateNumbers.java
@@ -0,0 +1,8 @@
+package com.project.calculate;
+
+public class CalculateNumbers {
+	public int Add(String numbers) {
+		int sumOfNumbers = 0;
+		return sumOfNumbers;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module stringCalculator {
+	requires org.junit.jupiter.api;
+}
