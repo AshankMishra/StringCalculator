3b02f56ae28df9994002d43e81f889b08df5f3a1
AshankMishra <ashankkumarmishra@gmail.com>
2022-04-13 11:08:26 +0530

    Changes:
    Added check for null and empty strings

--- src/com/project/calculate/CalculateNumbers.java
+++ src/com/project/calculate/CalculateNumbers.java
@@ -1,7 +1,13 @@
 package com.project.calculate;
 
 public class CalculateNumbers {
-	public int Add(String numbers) {
+	public static int Add(String numbers) {
+		if (numbers == null) {
+			throw new NullPointerException("String cannot be null");
+		}
+		if (numbers.isEmpty()) {
+			throw new IllegalArgumentException("String cannot be empty");
+		}
 		int sumOfNumbers = 0;
 		return sumOfNumbers;
 	}
--- test/com/project/calculate/CalculateNumbersTest.java
+++ test/com/project/calculate/CalculateNumbersTest.java
@@ -2,6 +2,7 @@ package com.project.calculate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculateNumbersTest {
@@ -11,4 +12,21 @@ class CalculateNumbersTest {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void testNullPointerException() {
+		NullPointerException thrown = Assertions.assertThrows(NullPointerException.class, () -> {
+			String numbers = null;
+			CalculateNumbers.Add(numbers);
+		});
+		Assertions.assertEquals("String cannot be null", thrown.getMessage());
+	}
+
+	@Test
+	void testEmptyString() {
+		IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			String numbers = "";
+			CalculateNumbers.Add(numbers);
+		});
+		Assertions.assertEquals("String cannot be empty", exception.getMessage());
+	}
 }
